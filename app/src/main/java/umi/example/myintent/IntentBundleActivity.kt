c73201ca8458a50ad3c054d9fa3ba0c11178e75f
deni1297 <ditata456@gmail.com>
2019-04-23 17:35:19 +0700

    first

---
+++ app/src/main/java/umi/example/myintent/IntentBundleActivity.kt
@@ -0,0 +1,33 @@
+package umi.example.myintent
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_intent_bundle.*
+
+class IntentBundleActivity  : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intent_bundle)
+
+        val namapanggilan = intent.getStringExtra("Nama Panggilan")
+        val ttl = intent.getStringExtra("TTL")
+        val alamatrumah = intent.getStringExtra("Alamat Rumah")
+        val hobby = intent.getStringExtra("Hobby")
+        val agama = intent.getStringExtra("Agama")
+        val email = intent.getStringExtra("Email")
+        val foto = intent.getStringExtra("FOTO")
+
+
+        Glide.with(this).load(foto).override(512, 512).error(R.drawable.hanum).into(ivProfil)
+        tvnamapanggilan.text = namapanggilan
+        tvttl.text = ttl
+        tvalamatrumah.text = alamatrumah
+        tvhobby.text = hobby
+        tvagama.text = agama
+        tvemail.text = email
+
+
+    }
+}
---
+++ app/src/main/java/umi/example/myintent/MainActivity.kt
@@ -0,0 +1,52 @@
+package umi.example.myintent
+
+import android.Manifest
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.kotlinpermissions.KotlinPermissions
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        KotlinPermissions.with(this) // where this is an FragmentActivity instance
+            .permissions(Manifest.permission.CAMERA)
+            .onAccepted { permissions ->
+                //List of accepted permissions
+            }
+            .onDenied { permissions ->
+                //List of denied permissions
+            }
+            .onForeverDenied { permissions ->
+                //List of forever denied permissions
+            }
+            .ask()
+        btnPindahActivityExplisit.setOnClickListener {
+            startActivity(Intent(this, ExplisitIntentActivity::class.java))
+        }
+        btnPindahActivityImplisit.setOnClickListener {
+            startActivity(Intent (this,ImplisitIntentActivity::class.java ))
+        }
+        btnPindahActivityIntentBundle.setOnClickListener {
+            val intent : Intent = Intent (this, IntentBundleActivity::class.java)
+            intent.putExtra("Nama Panggilan", "Hanum")
+            intent.putExtra("TTL", "Batang,05 Desember 1998")
+            intent.putExtra("Alamat Rumah", "Ds.Kembanglangit Kec.Blado Kab.Batang")
+            intent.putExtra("Hobby", "Membaca,Menulis,Menonton")
+            intent.putExtra("Agama", "Islam")
+            intent.putExtra("Email", "ismihanum05@gmail.com")
+            intent.putExtra("FOTO", "https://drive.google.com/drive/u/0/my-drive")
+
+            startActivity(intent)
+
+        }
+    }
+
+}
+
+
